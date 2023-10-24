d027bb359b355575bae207a68a686c5d18f85ef7
MATEUS KATER POMBEIRO <mkp19@inf.ufpr.br>
2023-10-24 00:16:25 -0300

    script consertado

--- ICC_Ep04/run.sh
+++ ICC_Ep04/run.sh
@@ -1,14 +1,18 @@
-echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+# echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
-for n in (64, 100, 128,  200, 256, 512, 600, 900, 1024, 2000, 2048, 3000, 4000) do
-    echo ${n}\t >> matvet
-    echo ${n}\t >> matmat
-    # likwid-perfctr -C 3 -g ENERGY -m ./matmult < ${n} > aux
-    ./matmult < ${n} > aux
-    cat aux | cut -f' ' -d 1 >> matvet
-    cat aux | cut -f' ' -d 2 >> matmat
-    echo \n >> matvet
-    echo \n >> matmat
+touch matvet
+touch matmat
+touch aux
+for n in 64 100 128 200 256 512 600 900 1024 2000 2048 3000 4000 
+do
+    echo "$n\t" >> matvet
+    echo "$n\t" >> matmat
+    # likwid-perfctr -C 3 -g ENERGY -m ./matmult < $n > aux
+    ./matmult $n > aux
+    cat aux | cut -d' ' -f1 >> matvet
+    cat aux | cut -d' ' -f2 >> matmat
+    echo "\n" >> matvet
+    echo "\n" >> matmat
 done
 
-echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+# echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
