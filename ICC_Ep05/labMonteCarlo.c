7172be5670c74250f577cdac24ff24277ff96e9b
kater-wsl <kater@DESKTOP-3BL9JVG>
2023-11-12 14:37:39 -0300

    monte carlo added

--- ICC_Ep05/labMonteCarlo.c
+++ ICC_Ep05/labMonteCarlo.c
@@ -9,17 +9,25 @@
 #define NRAND    ((double) rand() / RAND_MAX)  // drand48() 
 #define SRAND(a) srand(a) // srand48(a)
 
+double styblinski_tang(double x[10], int n) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        sum += 0.5 * (pow(x[i], 4) - 16 * pow(x[i], 2) + 5 * x[i]);
+    }
+    return sum;
+}
+
 // Integral Monte Carlo da função Styblinski-Tang de 2 variáveis
-double styblinskiTang(double a, double b, int namostras)
+double integral_monte_carlo(double a, double b, int namostras, int d)
 {
-  double resultado;
   double soma = 0.0;
   
   printf("Metodo de Monte Carlo (x, y).\n");
-  printf("a = (%f), b = (%f), n = (%d), variaveis = 2\n", a, b, namostras);
+  printf("a = (%f), b = (%f), n = (%d), variaveis = %d\n", a, b, namostras, d);
   
   double t_inicial = timestamp();
   
+
   /*
     
     AQUI IMPLEMENTE O CÁLCULO DA INTEGRAL  PELO
@@ -27,6 +35,22 @@ double styblinskiTang(double a, double b, int namostras)
     
   */
   
+  int count = 0;
+
+  for (int i = 0; i < namostras; i++) {
+      double point[10];
+      for (int j = 0; j < d; j++) {
+          point[j] = ((double)rand() / RAND_MAX) * (b - a) + a;
+      }
+
+      if (styblinski_tang(point, d) <= 0) {
+          count++;
+      }
+  }
+
+  double volume = pow(b - a, d);
+  double resultado = ((double)count / namostras) * volume;
+
   double t_final = timestamp();
   printf("Tempo decorrido: %f seg.\n", t_final - t_inicial);
   
@@ -70,7 +94,8 @@ int main(int argc, char **argv) {
   srandom(20232);
     
   // CHAMAR FUNÇÕES DE INTEGRAÇÃO E EXIBIR RESULTADOS
-  
+  double area = integral_monte_carlo(atoi(argv[1]), atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
+  printf("área = %lf\n", area);
   return 0;
 }
 
