324e6c226b733d1069af9b0c795c77ecdd781919
MATEUS KATER POMBEIRO <mkp19@inf.ufpr.br>
2023-11-17 16:07:52 -0300

    trabalho 2 adicionado

---
+++ Trabalho_02/main.c
@@ -0,0 +1,53 @@
+#include "calculoIntervalar.h"
+#include "operaEstrutura.h"
+#include "minimosQuadrados.h"
+#include "estruturas.h"
+#include "auxiliares.h"
+#include <likwid.h>
+
+/***************************
+ * Aluno: Joao Pedro Vicente Ramalho, GRR: 20224169
+ * Aluno: Mateus Kater Pombeiro, GRR: 20190366
+****************************/
+
+int main() {
+
+    LIKWID_MARKER_INIT;
+
+    // Aloca as estruturas de dados necessárias e guarda a tabela de pontos
+    struct ajustePol* sistema = montarEstrutura();
+    double start, end, tsolSL, tgeraSL;
+    LIKWID_MARKER_START("minQuad");
+
+    start = timestamp();
+    sistema = minimosQuadrados(sistema); //Aplica o método dos mínimos quadrados para ajuste de curva
+    end = timestamp();
+
+    LIKWID_MARKER_STOP("minQuad");
+    tgeraSL = end-start;
+
+    LIKWID_MARKER_START("gauss");
+    start = timestamp();
+    gauss(sistema);
+    end = timestamp();
+    LIKWID_MARKER_START("gauss");
+    tsolSL = end-start;
+    
+    // Imprime os coeficientes do polinômio
+    for (int i = 0; i < sistema->grauPol; i++)
+        printf("[%1.8e,%1.8e]  ", sistema->coeficientes[i].anterior, sistema->coeficientes[i].posterior);
+    printf("\n");
+
+    // Calcula e imprime os resíduos
+    calculaResiduos(sistema);
+
+    LIKWID_MARKER_CLOSE;
+
+    printf("%lf\n", tgeraSL);
+    printf("%lf\n", tsolSL);
+
+    // Libera a memória alocada nas structs
+    destruirEstrutura(sistema);
+  
+    return 0;
+}
---
+++ Trabalho_02/minimosQuadrados.h
@@ -0,0 +1,28 @@
+/***************************
+ * Aluno: Joao Pedro Vicente Ramalho, GRR: 20224169
+ * Aluno: Mateus Kater Pombeiro, GRR: 20190366
+****************************/
+
+#ifndef _MINIMOSQUADRADOS_
+#define _MINIMOSQUADRADOS_
+
+// Aplica o método dos mínimos quadrados e retorna a struct ajustePol modificada"
+struct ajustePol* minimosQuadrados(struct ajustePol* sistema);
+
+// Aplica o método de eliminação de gauss com pivoteamento parcial na matriz da struct ajustePol
+void gauss(struct ajustePol* sistema);
+
+// Varre a matriz da struct ajustePol e retorna o indice do maior valor double
+// presente na linha "i"
+int encontraMax(struct ajustePol* sistema, int i);
+
+// Efetua a troca entre as linhas "i" e "iPivo" da matriz da struct ajustePol
+void trocaLinha (struct ajustePol* sistema, int i, int iPivo);
+
+// Aplica a técnica de retro substituição na matriz da struct ajustePol
+void retroSubst(struct ajustePol* sistema);
+
+// Recebe uma struct operandos X0 e retorna f(X0)
+struct operandos polinomio(struct ajustePol* sistema, struct operandos x0);
+
+#endif
